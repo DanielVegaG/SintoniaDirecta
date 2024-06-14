c44c6cbe3e45f4a1797ad671f8cbf30f8ea7c5e8
Daniel Vega González <129546227+DanielVegaG@users.noreply.github.com>
2024-06-14 13:27:08 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,53 +1,114 @@
 import streamlit as st
-import base64
-import os
+import requests
 from pytube import YouTube, StreamQuery
 
-# Función para descargar y convertir el archivo de audio a MP3
-def download_file(stream):
-    title = stream.title + '.mp3'  # Cambiar la extensión a .mp3
-    stream.download(filename=title)  # Descargar el archivo con extensión .mp3
+import base64
+import os
 
-    # Leer el archivo descargado y convertirlo a base64 para generar el enlace de descarga
-    with open(title, 'rb') as f:
-        bytes = f.read()
-        b64 = base64.b64encode(bytes).decode()
-        href = f'<a href="data:file/mp3;base64,{b64}" download=\'{title}\'>\
-            Click aquí para descargar {title}\
-        </a>'
-        st.markdown(href, unsafe_allow_html=True)
+def clear_text():
+    st.session_state["url"] = ""
+    st.session_state["mime"] = ""
+    st.session_state["quality"] = ""
 
-    os.remove(title)  # Eliminar el archivo MP3 después de generar el enlace de descarga
+def download_file(stream, fmt):
+    """  """
+    if fmt == 'audio':
+        title = stream.title + ' audio.'+ stream_final.subtype
+    else:
+        title = stream.title + '.'+ stream_final.subtype
 
-# Configuración de la página de Streamlit
-st.set_page_config(page_title="YouTube Downloader", layout="wide")
+    stream.download(filename=title)
+    
+    if 'DESKTOP_SESSION' not in os.environ: #and os.environ('HOSTNAME')=='streamlit':
+    
+        with open(title, 'rb') as f:
+            bytes = f.read()
+            b64 = base64.b64encode(bytes).decode()
+            href = f'<a href="data:file/zip;base64,{b64}" download=\'{title}\'>\
+                Si le das a este link, se te descargará tu canción \
+            </a>'
+            st.markdown(href, unsafe_allow_html=True)
 
-# Sidebar con los controles de la aplicación
+        os.remove(title)
+
+
+def can_access(url):
+    """ check whether you can access the video """
+    access = False
+    if len(url) > 0:
+        try:
+            tube = YouTube(url)
+            if tube.check_availability() is None:
+                access=True
+        except:
+            pass
+    return access
+
+def refine_format(fmt_type: str='audio') -> (str, bool):
+    """ """
+    if fmt_type == 'video':
+        fmt = 'video'
+        progressive = True
+    else:
+        fmt = 'audio'
+        progressive = False
+
+    return fmt, progressive
+
+
+st.set_page_config(page_title=" Sintonía Directa", layout="wide")
+
+# ====== SIDEBAR ======
 with st.sidebar:
-    st.title("Aplicación para descargar desde YouTube")
 
-    # Entrada para la URL de YouTube
-    url = st.text_input("Inserta el enlace aquí:", key="url")
+    st.title("Descargador de música de YouTube")
 
-    # Selección del tipo de formato a descargar
-    fmt_type = st.selectbox("Elige el formato:", ['audio'], key='fmt')
+    url = st.text_input("Pon aquí el enlace:", key="url")
 
-# Página principal donde se muestra el video de YouTube si la URL es válida
-if url and YouTube(url).check_availability() is not None:
+    fmt_type = st.selectbox("Escoge el formato:", ['audio (only)', 'video'], key='fmt')
+
+    fmt, progressive = refine_format(fmt_type)
+
+    if can_access(url):
+
+        tube = YouTube(url)
+
+        streams_fmt = [t for t in tube.streams if t.type==fmt and t.is_progressive==progressive]
+
+        mime_types = set([t.mime_type for t in streams_fmt])
+        mime_type = st.selectbox("Mime types:", mime_types, key='mime')
+
+        streams_mime = StreamQuery(streams_fmt).filter(mime_type=mime_type)
+
+        # quality is average bitrate for audio and resolution for video
+        if fmt=='audio':
+            quality = set([t.abr for t in streams_mime])
+            quality_type = st.selectbox('Choose average bitrate: ', quality, key='quality')
+            stream_quality = StreamQuery(streams_mime).filter(abr=quality_type)
+        elif fmt=='video':
+            quality = set([t.resolution for t in streams_mime])
+            quality_type = st.selectbox('Choose resolution: ', quality, key='quality')
+            stream_quality = StreamQuery(streams_mime).filter(res=quality_type)
+
+        # === Download block === #
+        if stream_quality is not None:
+            stream_final = stream_quality[0]
+            if 'DESKTOP_SESSION' in os.environ:
+                download = st.button("Descargar canción", key='download')
+            else:
+                download = st.button("Get download link", key='download')
+
+            if download:
+                download_file(stream_final, fmt)
+                st.success('Success download!')
+                st.balloons()
+
+        st.button("Clear all address boxes", on_click=clear_text)
+
+
+# ====== MAIN PAGE ======
+
+if can_access(url):
+    if streams_fmt is None:
+        st.write(f"No {fmt_type} stream found")
     st.video(url)
-
-    tube = YouTube(url)
-    streams_fmt = [t for t in tube.streams.filter(type='audio', progressive=False)]
-
-    mime_types = set([t.mime_type for t in streams_fmt])
-    mime_type = st.selectbox("Tipos de MIME:", mime_types, key='mime')
-
-    streams_mime = StreamQuery(streams_fmt).filter(mime_type=mime_type)
-
-    # Botón para descargar el archivo
-    if st.button("Descargar archivo", key='download'):
-        if streams_mime:
-            stream_final = streams_mime.first()
-            download_file(stream_final)
-            st.success('¡Descarga exitosa!')
-            st.balloons()
